b298e1ad6d3c26e05fa568585336e2110b5daf36
Jeffrey Lee <jel347@ucsd.edu>
2017-04-07 15:33:35 -0700

    Added simple dependency installer script

---
+++ web_summary/massin.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+# mass installs dependencies from 'requirements.txt' (requires pip)
+
+import pip
+
+if __name__ == '__main__':
+
+	with open("requirements.txt") as f:
+		packages = f.readlines()
+
+	packages = [p.strip() for p in packages]
+
+	for p in packages:
+		pip.main(['install', package])
